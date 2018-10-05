460721ccfc5d94e8300ab1bf81b8fa0713050f35
Cristiano Mesquita <contact@cristianomesquita.com>
2018-10-05 15:27:57 -0300

    add: codepush button

--- App.js
+++ App.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import codePush from 'react-native-code-push'
 
 export default class App extends Component {
+  onButtonPress() {
+    codePush.sync({
+      updateDialog: true,
+      installMode: codePush.InstallMode.IMMEDIATE
+    })
+  }
   render() {
-    return <Text style={styles.welcome}>Testing codepush</Text>
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Some text to update</Text>
+        <TouchableOpacity style={styles.btn} onPress={this.onButtonPress}>
+          <Text>Check for updates</Text>
+        </TouchableOpacity>
+      </View>
+    )
   }
 }
 
 const styles = StyleSheet.create({
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
+  container: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 25
+  },
+  btn: {
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 10
   }
 })
